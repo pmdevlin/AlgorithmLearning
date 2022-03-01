3b77a0842db168a0148bd1e2e6c38a30d65dc07e
Paul Devlin <pmdevlin360@gmail.com>
2022-03-01 11:17:16 -0600

    working on bubbleSort

--- algorithms/sorting/bubbleSort.js
+++ algorithms/sorting/bubbleSort.js
@@ -1,20 +1,19 @@
- const arr = [8,9,5,4,3,2,9]
-// //     j           i
+const arr = [8,9,5,4,3,2,9]
+const array = [10,5,3,9,1,7,8,2,4,6]
 
-function bubbleSort(arr){
-    //outer loop 
-    for(let i = arr.length-1; i >= 2 ; i--){
-        //inner loop 
-        for(let j = 0; j < i - 1; j++){
+
+const bubbleSort = (arr) => {
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length; j++){
             if(arr[j] > arr[j + 1]){
                 let temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
             }
         }
-
     }
-  console.log(arr); 
+    return arr
 }
 
-bubbleSort(arr)
+console.log(bubbleSort(array))
