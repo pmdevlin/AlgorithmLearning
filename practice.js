0cf2beccd55f7cccc24b2a68f2dc041707fa496d
Paul Devlin <pmdevlin360@gmail.com>
2022-04-11 13:31:35 -0500

    working on roman numeral converter

---
+++ practice.js
@@ -0,0 +1,54 @@
+
+// const obj = {
+//     'I' : 1,
+//     'V' : 5,
+//     'X' : 10,
+//     'L' : 50,
+//     'C' : 100,
+//     'D' : 500,
+//     'M' : 1000
+// }
+
+
+const s = 'III'
+// const s = 'LVIII'
+
+const romanToInt = function(s) {
+    const obj = {
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000
+    }
+
+    for(let i = 0; i < s.length; i++){
+        const current = obj[s[i]]
+        const next = obj[s[i+1]]
+
+        if()
+    }
+
+
+    // const value = []
+    // const arr = s.split('')
+    // for(let i = 0; i < arr.length; i++){
+    //     const current = arr[i]
+    //     for( let key in obj){
+
+
+
+    //         if(current === key){
+    //             value.push(obj[key]) 
+
+    //         }
+    //     }
+    // }
+    // const initialValue = 0;
+    // const sum = value.reduce((prev, curr) => prev + curr, initialValue)
+    // return sum
+};
+
+console.log(romanToInt(s))
