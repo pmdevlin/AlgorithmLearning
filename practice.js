772a532cd5896a32e8fa0111315d80375d2ede92
Paul Devlin <pmdevlin360@gmail.com>
2022-04-11 13:49:10 -0500

    corrections

--- practice.js
+++ practice.js
@@ -10,26 +10,29 @@
 // }
 
 
-const s = 'III'
+// const s = 'III'
 // const s = 'LVIII'
 
-const romanToInt = function(s) {
-    const obj = {
-        'I' : 1,
-        'V' : 5,
-        'X' : 10,
-        'L' : 50,
-        'C' : 100,
-        'D' : 500,
-        'M' : 1000
-    }
+// const romanToInt = function(s) {
+//     const obj = {
+//         'I' : 1,
+//         'V' : 5,
+//         'X' : 10,
+//         'L' : 50,
+//         'C' : 100,
+//         'D' : 500,
+//         'M' : 1000
+//     }
+// const value = 0
 
-    for(let i = 0; i < s.length; i++){
-        const current = obj[s[i]]
-        const next = obj[s[i+1]]
+//     for(let i = 0; i < s.length; i++){
+//         const current = obj[s[i]]
+//         const next = obj[s[i+1]]
 
-        if()
-    }
+//         if(current < next){
+//             value = value + next - current
+//         }
+//     }
 
 
     // const value = []
@@ -49,6 +52,18 @@ const romanToInt = function(s) {
     // const initialValue = 0;
     // const sum = value.reduce((prev, curr) => prev + curr, initialValue)
     // return sum
-};
+// };
 
-console.log(romanToInt(s))
+// console.log(romanToInt(s))
+
+
+
+function add (num) {
+    let sum 
+    for(let i = 0; i < num; i++){
+         
+    }
+   
+}
+
+add(10)
