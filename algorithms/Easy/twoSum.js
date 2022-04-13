671984a6e8ab0c1194de52555844e237adc6ef41
Paul Devlin <pmdevlin360@gmail.com>
2022-04-13 17:11:34 -0500

    updates to twoSum

--- algorithms/Easy/twoSum.js
+++ algorithms/Easy/twoSum.js
@@ -2,8 +2,10 @@
 // Given an array or numbers and a number find the first two numbers that add up to the given number 
 
 //data
-const arr = [ 1,2,3,4,5,5,6,2,3,4,9]
-let num = 11
+//const arr = [ 1,2,3,4,5,5,6,2,3,4,9]
+//const arr = []
+const arr = [5]
+let num = 13
 
 
 // two sum function takes in an array and number
@@ -21,9 +23,10 @@ const twoSum = (arr, num) => {
         if(!obj[number]){
             obj[current] = i 
         }else{
-            return [number, current]
+            return [obj[number], i]
         }
     }
+    return null 
 }
 
 console.log(twoSum(arr, num))
@@ -32,8 +35,9 @@ console.log(twoSum(arr, num))
 
 //Two Sum 
 // brute force / two pointer technique 
-const array = [1,3,7,9,2]
-const target = 11
+//const array = [1,3,7,9,2]
+//const array = []
+//const target = 11
 
 const twoSum2 = (arr, t ) => {
     for(let i = 0; i < arr.length; i++){
@@ -48,4 +52,23 @@ const twoSum2 = (arr, t ) => {
     return null
 }
 
-console.log(twoSum2(array, target))
+// console.log(twoSum2(array, target))
+
+
+//const arr = [1,3,7,9,2]
+const target = 11
+
+const twoSum3= (num, target) => {
+    const obj = {}
+    for(let i = 0; i < num.length; i++){
+       const current = obj[num[i]]
+       if(current >= 0){
+            return [current, i]
+       }else{
+           const numToFind = target - num[i]
+           obj[numToFind] = i
+       }
+    }
+    return null
+}
+//console.log(twoSum3(arr, target))
