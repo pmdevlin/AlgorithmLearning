a619015ff48423cb5b89df6a58976625cab9d6d1
Paul Devlin <pmdevlin360@gmail.com>
2022-08-11 15:55:01 -0500

    file changes

--- dataStructures/LinkedListReps.js
+++ dataStructures/LinkedListReps.js
@@ -24,9 +24,36 @@ class LinkedList {
     this.length++;
     return this;
   }
+  newTail(value) {
+    if (!this.head) {
+      this.newHead(value);
+    } else {
+      const newNode = new Node(value);
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  print() {
+    const list = [];
+    let count = 0;
+    let current = this.head;
+    while (count < this.length) {
+      list.push(current.data);
+      current = current.next;
+      count++;
+    }
+    return list;
+  }
 }
 
 const list = new LinkedList();
 list.newHead(10);
 list.newHead(11);
+
+list.newTail(100);
+
+console.log(list.print());
 console.log(list);
