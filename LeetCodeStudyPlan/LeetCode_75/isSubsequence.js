82706bd8d95469d27ecd7d33a6da7b8ec7da8612
Paul Devlin <pmdevlin360@gmail.com>
2022-08-15 05:42:07 -0500

    adding is Subsequence

--- LeetCodeStudyPlan/LeetCode_75/isSubsequence.js
+++ LeetCodeStudyPlan/LeetCode_75/isSubsequence.js
@@ -5,8 +5,8 @@
 // remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 
 // Example 1:
-// let s = "abc",
-//   t = "ahbgdc";
+let s = "abc",
+  t = "ahbgdc";
 // Output: true
 
 // Example 2:
@@ -15,31 +15,23 @@
 // Output: false
 
 //failure:
-let s = "acb",
-  t = "ahbgdc";
+// let s = "acb",
+//   t = "ahbgdc";
 
-// thought process subtract letters form t and add them to an arr
-// if the two arrays are equal then return true otherwise false
+// two pointer solution to check with one evolution of the strings
 
-const isSubsequence = (s, t) => {
-  const map = new Map();
+const isSubsequence = function (s, t) {
+  let i = 0,
+    j = 0;
 
-  for (let i = 0; i < s.length; i++) {
-    map.set(s[i], null);
-
-    // for (let j = 0; j < t.length; j++) {
-    //   if (s[i] === t[j]) {
-    //     map.set(s[i], t[j]);
-    //   }
-    // }
+  while (j < t.length && i < s.length) {
+    if (s[i] === t[j]) {
+      i++;
+    }
+    j++;
   }
-  //   if (map.size === s.length) {
-  //     console.log(map);
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
-  console.log(map);
+  return i === s.length ? true : false;
 };
 
+//console.log(isSubsequence(s, t));
 console.log(isSubsequence(s, t));
