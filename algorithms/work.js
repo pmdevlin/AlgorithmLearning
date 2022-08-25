8d946a2f1afa46c644426435f9b4bf30021c7249
Paul Devlin <pmdevlin360@gmail.com>
2022-08-25 09:41:23 -0500

    changed to an obj for interview challange

--- algorithms/work.js
+++ algorithms/work.js
@@ -6,85 +6,148 @@ const array1 = ["a", "D", "L", "s", "C", "b"];
 
 // parameters (arr, asc, desc)
 
-const acsSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
-};
-
-const decsSort = (arr) => {
-  let temp;
-  let flag = true;
-  while (flag) {
-    flag = false;
+const sorter = {
+  acsSort: function (arr) {
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < arr[i + 1]) {
-        temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        flag = true;
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > arr[j + 1]) {
+          let temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
       }
     }
-  }
-  return arr;
-};
-
-const numberArrayTest = (arr) => {
-  let num = 0;
-  let numberArray = false;
-
-  while (num <= arr.length) {
-    if (typeof arr[num] === "number") {
-      num++;
+    console.log(arr);
+  },
+  decsSort: function (arr) {
+    let temp;
+    let flag = true;
+    while (flag) {
+      flag = false;
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < arr[i + 1]) {
+          temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          flag = true;
+        }
+      }
+    }
+    console.log(arr);
+  },
+  numberArrayTest: function (arr) {
+    let num = 0;
+    while (num <= arr.length) {
+      if (typeof arr[num] === "number") {
+        num++;
+      }
     }
     if (num === arr.length) {
       console.log("pure number array");
-      numberArray = true;
-      return numberArray;
+    } else {
+      console.log("not a pure number function");
     }
-  }
-
-  return false;
+  },
+  // stringArrayTest: function (arr) {
+  //   let letter = 0;
+  //   let stringArray = false;
+  //   while (letter <= arr.length) {
+  //     if (typeof arr[letter] === "string") {
+  //       let current = arr[letter];
+  //       current.toLowerCase();
+  //       letter++;
+  //     }
+  //     if (letter === arr.length) {
+  //       stringArray = true;
+  //       console.log("pure string array");
+  //       return stringArray;
+  //     }
+  //   }
+  //   return false;
+  // },
 };
 
-const stringArrayTest = (arr) => {
-  let letter = 0;
-  let stringArray = false;
+sorter.numberArrayTest(array);
+sorter.acsSort(array);
+sorter.decsSort(array);
 
-  // add another if else
+sorter.numberArrayTest(array1);
 
-  while (letter <= arr.length) {
-    if (typeof arr[letter] === "string") {
-      let current = arr[letter];
-      current.toLowerCase();
-      letter++;
-    }
-    if (letter === arr.length) {
-      stringArray = true;
-      console.log("pure string array");
-      return stringArray;
-    }
-  }
-  return false;
-};
+// const acsSort = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// };
 
-console.log(stringArrayTest(array));
+// const decsSort = (arr) => {
+//   let temp;
+//   let flag = true;
+//   while (flag) {
+//     flag = false;
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] < arr[i + 1]) {
+//         temp = arr[i];
+//         arr[i] = arr[i + 1];
+//         arr[i + 1] = temp;
+//         flag = true;
+//       }
+//     }
+//   }
+//   return arr;
+// };
 
-const allSorts = (arr, boolValue) => {
-  if (boolValue === true) {
-    return acsSort(arr);
-  }
-  if (boolValue === false) {
-    return decsSort(arr);
-  }
-};
+// const numberArrayTest = (arr) => {
+//   let num = 0;
+//   let numberArray = false;
+
+//   while (num <= arr.length) {
+//     if (typeof arr[num] === "number") {
+//       num++;
+//     }
+//     if (num === arr.length) {
+//       console.log("pure number array");
+//       numberArray = true;
+//       return numberArray;
+//     }
+//   }
+
+//   return false;
+// };
+
+// const stringArrayTest = (arr) => {
+//   let letter = 0;
+//   let stringArray = false;
+
+//   // add another if else
+
+//   while (letter <= arr.length) {
+//     if (typeof arr[letter] === "string") {
+//       let current = arr[letter];
+//       current.toLowerCase();
+//       letter++;
+//     }
+//     if (letter === arr.length) {
+//       stringArray = true;
+//       console.log("pure string array");
+//       return stringArray;
+//     }
+//   }
+//   return false;
+// };
+
+// console.log(stringArrayTest(array));
+
+// const allSorts = (arr, boolValue) => {
+//   if (typeof arr[0] === "number") {
+//   }
+// };
 
 // const result = allSorts(array, false);
 // console.log(result);
--- myfile/myclock/index.js
+++ myfile/myclock/index.js
@@ -1,25 +1,64 @@
-// create and algorithm that can workout the hours worked
-//through a 24 hour period
+let hourDigit1 = document.querySelector("#hourNum1");
+let hourDigit2 = document.querySelector("#hourNum2");
+let minDigit1 = document.querySelector("#minNum1");
+let minDigit2 = document.querySelector("#minNum2");
+let secDigit1 = document.querySelector("#secNum1");
+let secDigit2 = document.querySelector("#secNum2");
 
-// parameters hour of start to hour of finish
-// data the 24 hour clock
+const nodeArr = [
+  hourDigit1,
+  hourDigit2,
+  minDigit1,
+  minDigit2,
+  secDigit1,
+  secDigit2,
+];
 
-import clock from "./clockObj.js";
+const addDigit = (arr) => {
+  arr.map((elem) => {
+    elem.innerHTML = "0";
+  });
+};
+addDigit(nodeArr);
 
-const getHour = (time) => {
-  return clock[time];
+const secondsTimerFunction = () => {
+  let count = 0;
+  secDigit2.innerHTML = count;
+  let sec = (secDigit1.innerHTML = 0);
+  setTimeout(() => {
+    if (count === 60) {
+      count = 0;
+      sec++;
+    }
+    count++;
+  });
 };
 
-const hoursWorked = (start, end, breaks) => {
-  if (start === "") {
-    return "enter a value for start";
-  }
-  if (end === "") {
-    return "enter a value for end";
-  }
+secondsTimerFunction();
 
-  return getHour(end) - getHour(start) - breaks;
-};
+// addDigit(hourDigit1);
+// addDigit(hourDigit2);
+// addDigit(minDigit1);
+// addDigit(minDigit2);
+// addDigit(secDigit1);
+// addDigit(secDigit2);
 
-let result = hoursWorked("5:00am", "4:00pm", 2);
-console.log(result);
+//import clock from "./clockObj.js";
+
+// const getHour = (time) => {
+//   return clock[time];
+// };
+
+// const hoursWorked = (start, end, breaks) => {
+//   if (start === "") {
+//     return "enter a value for start";
+//   }
+//   if (end === "") {
+//     return "enter a value for end";
+//   }
+
+//   return getHour(end) - getHour(start) - breaks;
+// };
+
+// let result = hoursWorked("5:00am", "4:00pm", 2);
+// console.log(result);
