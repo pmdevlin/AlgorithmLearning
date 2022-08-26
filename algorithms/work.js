4fc4bd83aa554bf8adc571860217a8cf2d9b4085
Paul Devlin <pmdevlin360@gmail.com>
2022-08-25 20:22:08 -0500

    adding

--- algorithms/work.js
+++ algorithms/work.js
@@ -5,6 +5,7 @@ const array = [5, 2, 4, 1, 7, 8];
 const array1 = ["a", "D", "L", "s", "C", "b"];
 
 // parameters (arr, asc, desc)
+//console.log(typeof array1[1]);
 
 const sorter = {
   acsSort: function (arr) {
@@ -37,117 +38,39 @@ const sorter = {
   },
   numberArrayTest: function (arr) {
     let num = 0;
-    while (num <= arr.length) {
+    let str = 0;
+    let other = 0;
+    while (num <= arr.length || str <= arr.length || other <= arr.length) {
       if (typeof arr[num] === "number") {
         num++;
+        console.log(num);
+      } else if (typeof arr[num] === "string") {
+        str++;
+      } else {
+        other++;
       }
     }
+    console.log(num);
+    console.log(str);
+    console.log(other);
+
     if (num === arr.length) {
       console.log("pure number array");
+      return true;
+    } else if (str === arr.length) {
+      console.log("pure string array");
+    } else if (other === arr.length) {
+      console.log("pure array of different type");
+      return false;
     } else {
       console.log("not a pure number function");
+      return false;
     }
   },
-  // stringArrayTest: function (arr) {
-  //   let letter = 0;
-  //   let stringArray = false;
-  //   while (letter <= arr.length) {
-  //     if (typeof arr[letter] === "string") {
-  //       let current = arr[letter];
-  //       current.toLowerCase();
-  //       letter++;
-  //     }
-  //     if (letter === arr.length) {
-  //       stringArray = true;
-  //       console.log("pure string array");
-  //       return stringArray;
-  //     }
-  //   }
-  //   return false;
-  // },
 };
 
 sorter.numberArrayTest(array);
-sorter.acsSort(array);
-sorter.decsSort(array);
-
 sorter.numberArrayTest(array1);
 
-// const acsSort = (arr) => {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = 0; j < arr.length; j++) {
-//       if (arr[j] > arr[j + 1]) {
-//         let temp = arr[j];
-//         arr[j] = arr[j + 1];
-//         arr[j + 1] = temp;
-//       }
-//     }
-//   }
-//   return arr;
-// };
-
-// const decsSort = (arr) => {
-//   let temp;
-//   let flag = true;
-//   while (flag) {
-//     flag = false;
-//     for (let i = 0; i < arr.length; i++) {
-//       if (arr[i] < arr[i + 1]) {
-//         temp = arr[i];
-//         arr[i] = arr[i + 1];
-//         arr[i + 1] = temp;
-//         flag = true;
-//       }
-//     }
-//   }
-//   return arr;
-// };
-
-// const numberArrayTest = (arr) => {
-//   let num = 0;
-//   let numberArray = false;
-
-//   while (num <= arr.length) {
-//     if (typeof arr[num] === "number") {
-//       num++;
-//     }
-//     if (num === arr.length) {
-//       console.log("pure number array");
-//       numberArray = true;
-//       return numberArray;
-//     }
-//   }
-
-//   return false;
-// };
-
-// const stringArrayTest = (arr) => {
-//   let letter = 0;
-//   let stringArray = false;
-
-//   // add another if else
-
-//   while (letter <= arr.length) {
-//     if (typeof arr[letter] === "string") {
-//       let current = arr[letter];
-//       current.toLowerCase();
-//       letter++;
-//     }
-//     if (letter === arr.length) {
-//       stringArray = true;
-//       console.log("pure string array");
-//       return stringArray;
-//     }
-//   }
-//   return false;
-// };
-
-// console.log(stringArrayTest(array));
-
-// const allSorts = (arr, boolValue) => {
-//   if (typeof arr[0] === "number") {
-//   }
-// };
-
-// const result = allSorts(array, false);
-// console.log(result);
+// sorter.acsSort(array);
+// sorter.decsSort(array);
