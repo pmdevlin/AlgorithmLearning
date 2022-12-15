960d0c1e3c7bcafbc050507a2a71ce4366030153
pmdevlin <pmdevlin360@gmail.com>
2022-12-15 16:05:27 -0600

    test

---
+++ algorithms/test.js
@@ -0,0 +1,52 @@
+const skills = [
+  {
+    name: "JavaScript",
+    percentage: "80%",
+    logo: "IoLogoJavascript",
+  },
+  {
+    name: "Java",
+    percentage: "45%",
+    logo: "FaJava",
+  },
+  {
+    name: "HTML",
+    percentage: "90%",
+    logo: "FaHtml5",
+  },
+  {
+    name: "CSS",
+    percentage: "90%",
+    logo: "SiCss3",
+  },
+
+  {
+    name: "React",
+    percentage: "80%",
+    logo: "FaReact",
+  },
+  {
+    name: "Node",
+    percentage: "75%",
+    logo: "FaNode",
+  },
+  {
+    name: "Postgresql",
+    percentage: "70%",
+    logo: "SiPostgresql",
+  },
+];
+
+// skills.forEach((elem, index, array) => {
+//   console.log(elem);
+//   console.log(index);
+//   console.log(array);
+// });
+
+const skillGen = (skills) => {
+  skills.map((elem) => {
+    elem.name;
+  });
+};
+
+skillGen(skills);
